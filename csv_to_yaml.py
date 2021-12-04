902fa8cdadbd2def7ceb53b25870f43ed2478387
Sangam Biradar <sangam14@users.noreply.github.com>
2021-12-05 04:35:07 +0530

    Update csv_to_yaml.py

--- csv_to_yaml.py
+++ csv_to_yaml.py
@@ -1,5 +1,6 @@
-# Takes a file CSV file called "data.csv" and outputs each row as a numbered YAML file.
+# Takes a file CSV file called "data.csv" and outputs each row as a YAML file named after first column.
 # Data in the first row of the CSV is assumed to be the column heading.
+# Original work borrowed from: https://github.com/hfionte/csv_to_yaml
 
 # Import the python library for parsing CSV files.
 import csv
@@ -22,12 +23,14 @@ for row_index, row in enumerate(datareader):
 
 	# Othrwise, create a YAML file from the data in this row...
 	else:
-		# Open a new file with filename based on index number of our current row.
-		filename = str(row_index) + '.yml'
+		# Open a new file with filename based on the first column
+		filename = row[0].lower().replace(" county", "").replace(" ", "_") + '.md'
 		new_yaml = open(filename, 'w')
 
 		# Empty string that we will fill with YAML formatted text based on data extracted from our CSV.
 		yaml_text = ""
+		yaml_text += "---\n"
+		yaml_text += "layout: county \n"
 
 		# Loop through each cell in this row...
 		for cell_index, cell in enumerate(row):
@@ -35,14 +38,14 @@ for row_index, row in enumerate(datareader):
 			# Compile a line of YAML text from our headings list and the text of the current cell, followed by a linebreak.
 			# Heading text is converted to lowercase. Spaces are converted to underscores and hyphens are removed.
 			# In the cell text, line endings are replaced with commas.
-			cell_heading = data_headings[cell_index].lower().replace(" ", "_").replace("-", "")
+			cell_heading = data_headings[cell_index].lower().replace(" ", "_").replace("-", "_").replace("%", "percent").replace("$", "").replace(",", "")
 			cell_text = cell_heading + ": " + cell.replace("\n", ", ") + "\n"
 
 			# Add this line of text to the current YAML string.
 			yaml_text += cell_text
 
 		# Write our YAML string to the new text file and close it.
-		new_yaml.write(yaml_text)
+		new_yaml.write(yaml_text + "---\n")
 		new_yaml.close()
 
 # We're done! Close the CSV file.
